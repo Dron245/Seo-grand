155380e0b8dbc820672143aed8d901de2c72fdde
Dron245 <Dron245@yandex.ru>
2025-07-03 21:24:14 +0300

    charherone адаптация 768

--- chargerone/src/js/files/script.js
+++ chargerone/src/js/files/script.js
@@ -8,8 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
 	function documentActions(e) {
 		const targetElement = e.target;
 		console.log(targetElement);
-		if(targetElement.closest('.subsubmenu__top')) {
-			targetElement.closest('.subsubmenu__top').nextElementSibling.classList.toggle('_sub-sub-menu-active');
+		if (targetElement.closest(".subsubmenu__top")) {
+			const clickedTop = targetElement.closest(".subsubmenu__top");
+			const submenu = clickedTop.nextElementSibling;
+			const isActive = submenu.classList.contains("_sub-sub-menu-active");
+			document.querySelectorAll(".subsubmenu__top").forEach((el) => {
+				// el.classList.remove('_sub-sub-menu-active');
+				el.nextElementSibling.classList.remove("_sub-sub-menu-active");
+			});
+			if (!isActive) {
+				submenu.classList.add("_sub-sub-menu-active");
+			}
 		}
 	}
-})
+});
