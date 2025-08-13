a4e14a102c84c86cb65d64a25571362e8bfe2ec4
Dron245 <Dron245@yandex.ru>
2025-08-13 10:56:10 +0300

    aircon добавление новых примеров работ

---
+++ aircon-lux/Примеры работ/index.php
@@ -0,0 +1,82 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Примеры работ");
+?>
+
+    <section class="section title-block gray">
+        <div class="inner-page row row_bottom">
+            <div class="title-block__left">
+                <div class="left-menu">
+                    <h1 class="left-menu__ttl">Примеры работ</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?$APPLICATION->IncludeComponent(
+		"bitrix:news.list",
+		"works",
+		array(
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"CHECK_DATES" => "Y",
+			"DETAIL_URL" => "",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"DISPLAY_DATE" => "N",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"FIELD_CODE" => array(
+				0 => "",
+				1 => "",
+			),
+			"FILTER_NAME" => "",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+			"IBLOCK_ID" => "3",
+			"IBLOCK_TYPE" => "ivanychi_content",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"MESSAGE_404" => "",
+			"NEWS_COUNT" => "20",
+			"PAGER_BASE_LINK_ENABLE" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "N",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => ".default",
+			"PAGER_TITLE" => "Новости",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"PROPERTY_CODE" => array(
+				0 => "WORKS_TYPE",
+				1 => "",
+			),
+			"SET_BROWSER_TITLE" => "Y",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "Y",
+			"SET_META_KEYWORDS" => "Y",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "Y",
+			"SHOW_404" => "N",
+			"SORT_BY1" => "ACTIVE_FROM",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER1" => "DESC",
+			"SORT_ORDER2" => "ASC",
+			"STRICT_SECTION_CHECK" => "N",
+			"COMPONENT_TEMPLATE" => "works"
+		),
+		false
+	);?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
